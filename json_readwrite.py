49f59e81ff928179bc704edca0c78b44029b7570
Colin Dablain <colin.dablain@gmail.com>
2015-01-03 15:56:15 -0500

    Reorganized scripts, doesn't work as of now, but will add unit tests and make it work before commiting to github.  Added __init__.py to make the whole thing importable as a module

--- json_readwrite.py
+++ json_readwrite.py
@@ -1,22 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 
+
 def load(name):
-   f = open(name)
-   print(f)
-   data = json.load(f)
-   f.close()
-   return(data)
-   
+    f = open(name)
+    print f
+    data = json.load(f)
+    f.close()
+    return data
+
+
 def write(name, data):
-   filename = name + ".json"
-   f = open(filename, "w")
-   json.dump(data, f)
-   f.close()
-   
+    filename = name + '.json'
+    f = open(filename, 'w')
+    json.dump(data, f)
+    f.close()
+
+
 def exists(name):
-   try:
-      open(name)
-   except IOError:
-      return(False)
-   else:
-      return(True)
+    try:
+        open(name)
+    except IOError:
+        return False
+    else:
+        return True
+
+
--- quote.py
+++ quote.py
@@ -1,22 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
 import json
 
+
 class Quote(object):
-   """Class for describing a quote"""
-   def __init__(self, page, data, comment):
-      """Initializes a Quote & describes some basic things about it
-      
+
+    """Class for describing a quote"""
+
+    def __init__(
+            self,
+            page,
+            data,
+            comment,
+            ):
+        """Initializes a Quote & describes some basic things about it
+
          Parameters
          ----------
-         
+
          page  :  the page number the quote is from
          data  :  the quote itself
          comment  :  any comment the user has on the content of the quote
       """
-      self.page = page
-      self.data = data
-      self.comment = comment
-      
-q = Quote(12, "hello", "asdfasdfasdf")
-print json.dumps(q.__dict__)
+
+        self.page = page
+        self.data = data
+        self.comment = comment
+
+
+q = Quote(12, 'hello', 'asdfasdfasdf')
+print(json.dumps(q.__dict__))
 print(q.__dict__)
-      
